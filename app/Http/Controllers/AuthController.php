2269fe9e89046295756e96ffd66e4ee170de2e44
KiraGONW <aaronhp99@gmaii.com>
2021-05-18 12:05:26 +0100

    change

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,6 +16,7 @@ class AuthController extends Controller
     public function postTheme(Request $request){
         $user = User::whereEmail($request->email)->first();
         $user->otherTheme =! $user->otherTheme;
+        $user->save();
          return $user->otherTheme;
     }
     public function login(Request $request)
